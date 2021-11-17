65c1d88dcd7324167fc75c64a0b09477af31625c
vidya <punuogoti.vidyasagar@gmail.com>
2021-11-17 07:05:43 +0530

    Tokenization 1st commit

---
+++ Tokenization.py
@@ -0,0 +1,16 @@
+import nltk
+# nltk.download()
+
+para = """
+        Search is part of everyday life, especially considering the vast amounts of data generated in the modern interconnected world.
+        Azure Cognitive Search exists to compliment existing technologies and provides a programmable search engine built on Apache Lucene. 
+        Azure Cognitive Search is a highly available platform offering a 99.9% uptime SLA available for cloud and on-premises assets.
+
+        """
+
+sentences = nltk.sent_tokenize(para)
+print(sentences)
+words = nltk.word_tokenize(para)
+print(words)
+
+# words = nltk.sent
